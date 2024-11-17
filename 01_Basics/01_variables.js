c4773bd7bc6b0c42b27f8621922ec285782bd664
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-11-17 11:32:11 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+// const cant be cjanged 
+const accountId = 1223232
+let accountEmail = "Sandeep12@gmail.com"
+accountCity = "Jaipur"
+
+//   accountId = 2   // const cant change
+accountEmail = "sandeep@.com"
+accountPassword = " 2312312"
+accountCity = "Benguluru"
+let accountSatate ;
+
+// We avoid using var coz if you want to use anywhere other the same var it will change the prevoius one  , means it work Universally
+
+// use Const &&  let
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountSatate])
+
+
+
+
+
