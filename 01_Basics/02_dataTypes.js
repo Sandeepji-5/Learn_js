e978c4a6416af1e9f92074e361047acc5f5c377e
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-11-17 11:24:06 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer
+//alert( 3 + 3)  // we are using nodejs  not Browser
+
+//console.log(3+3) ;console.log("Sandeep")  // not readable 
+
+//ECMA-262  :- it write the standerfd of javascript
+
+let name = "Sandeep"
+let age = 18
+let isloggedIn = false
+let state = null;
+
+
+// number => 2 to powe 53
+// for larger number use = bigint
+// boolean => true/false
+// null => standalone  [representation of empty value]
+// undefined =>
+
+    // symbol => unique
+    // object 
+    console.log(typeof"Sandeep");
+    
