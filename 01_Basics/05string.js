4fff7b1d21068289fee074483652afa30ffc6a02
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-11-29 07:13:12 +0000

    String learning

---
+++ 01_Basics/05string.js
@@ -0,0 +1,27 @@
+// const name =  "hitesh"
+// const repoCount = 50;
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello My name is ${name} and my repo countn is ${repoCount}`)
+// const gameName =  new String('Sandeep')
+
+//  console.log(gameName[0]);
+//  console.log(gameName.__proto__);
+
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
+//  console.log(gameName.charAt(2));
+//  console.log(gameName.indexOf('t'));
+
+//  const newString  =  gameName.substring(0,4)
+//  console.log(newString)
+
+//  const anotherString = gameName.slice(0,4)
+//  console.log(anotherString);
+
+
+
+
+
+
