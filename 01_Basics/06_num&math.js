4c721f24902c40602767ebc6e8b2e385b4a70bc5
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-11-29 09:22:24 +0000

    ..

--- 01_Basics/06_num&math.js
+++ 01_Basics/06_num&math.js
@@ -1,4 +1,32 @@
- const balance  = new Number(100)
- console.log(balance);
+//  const balance  = new Number(100)
+//  console.log(balance);
 
+//  console.log(balance.toString().length)
+//  console.log(balance.toFixed(1));
+
+//  const otherNumber = 123.8977
+
+//  console.log(otherNumber.toPrecision(4));
+
+//  const hundreds = 10000;
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++MATH++++++++++++++++++++++++++++++++++++
  
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2))
+
+console.log(Math.random());// 0 - 1 ke beech number 
+console.log(Math.random()*10);// 
+console.log(Math.random()*10+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+console.log(Math.floor(1));
+
