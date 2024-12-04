0a9c3126a0e53bd777a95728f416634bf2117747
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-12-04 14:01:09 +0000

    .

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,13 @@
+// (function chai (){
+//     console.log('DB CONNECTED');
+// })();
+// ( () => {
+//     console.log(`DB CONNECTED TWO`);
+// })();
+
+
+
+( (name) => {
+    console.log(`DB  CONNECTED TWO  ${name}`);
+})("SANDEEP ")
+// if you write two IIFE then dont forget two write semicolon to the end of firstfunction
