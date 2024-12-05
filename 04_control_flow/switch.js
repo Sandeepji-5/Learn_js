983552843b67f9d152b9263f7f112e45b49b3bc4
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2024-12-05 11:25:03 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,75 @@
+// const month = 3;
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Default case switch");
+// }
+
+// const Month = "jan";
+// switch (Month){
+//     case "jan":
+//         console.log("January");
+//         break;
+//     case "f":
+//         console.log("feb");
+//         break;
+//     case "Ma":
+//         console.log("March");
+//         break;
+//     case "Ap":
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Default case switch");
+// }
+
+
+// falsy Values
+// {false  , 0,-0, BigInt , 0n, "", null, undefined, NaN}
+
+// truthy values 
+// "0" , 'false', " ",[] , {} ,function(){} ----empty function
+
+// const userEmail = "";
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log(" Object is empty ");
+// }
+
+
+// Nullish coalescing Operator (??) : null undefined 
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 565
+// val1 = null  ?? 89 ?? 30
+// console.log(val1);
+
+
+// Ternary operator
+// condition ? true : false;
+const iceTeaPrice = 100;
+iceTeaPrice >=80 ? console.log(" Less than 80") : console.log(" More than 80");
+
+
+
