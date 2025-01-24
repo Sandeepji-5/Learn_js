bcec462e8b03568c8343bcbc7a895c141e938be2
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2025-01-24 12:35:37 +0530

    Update one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,13 @@
-// for
+// for loop
 for(let index = 0; index < 10; index++){
-    const element =  index;
+    const element = index;
     console.log(element);
-
 }
+
+
+// The loop starts with index set to 0.
+// It continues to run as long as index is less than 10.
+// In each iteration, index is incremented by 1.
+// The current value of index is assigned to element and logged to the console.
+// The output will be the numbers 0 to 9 each on a new line.
+
